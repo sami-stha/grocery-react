078824d813ae180b19faa5e341a0f7069961e95c
Samikshya Shrestha <sami.college098@gmail.com>
2026-02-16 22:46:47 +0545

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
 import Items from "./components/Items";
+import Form from "./components/form";
 import { groceryItems } from "./data/groceryItems";
 import { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
+import { nanoid } from "nanoid";
+
 
 const App = () => {
    const [items, setItems] = useState(groceryItems);
+
+   const addItem = (itemName) => {
+    const newItem = {
+      name: itemName,
+      completed: false,
+      id: nanoid(),
+    };
+    const newItems = [...items, newItem];
+    setItems(newItems);
+    toast.success("grocery item added");
+  };
 
   const editCompleted = (itemId) => {
     const newItems = items.map((item) => {
@@ -27,6 +41,7 @@ const App = () => {
   return (
     <section className="section-center">
       <ToastContainer position="top-center" />
+       <Form addItem={addItem} />
       <Items
         items={items}
         editCompleted={editCompleted}
