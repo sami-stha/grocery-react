e85f242dc7581ced2d5449a35798fd40e581ceab
Samikshya Shrestha <sami.college098@gmail.com>
2026-02-16 22:02:12 +0545

    update

--- src/components/SingleItem.jsx
+++ src/components/SingleItem.jsx
@@ -1,7 +1,7 @@
 import { FiEdit, FiTrash2 } from "react-icons/fi";
 import "./SingleItem.css";
 
-const SingleItem = ({ item, editCompleted }) => {
+const SingleItem = ({ item, editCompleted, removeItem }) => {
   return (
     <div className="single-item">
       <input type="checkbox" checked={item.completed} onChange={() => editCompleted(item.id)} />
@@ -21,7 +21,7 @@ const SingleItem = ({ item, editCompleted }) => {
       <button
         className="btn icon-btn remove-btn"
         type="button"
-        onClick={() => null}
+        onClick={() => removeItem(item.id)}
       >
         <FiTrash2 size={18} />
       </button>
